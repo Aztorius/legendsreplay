a513da32280673b1bb2875041b985ae3349bb1d1
Aztorius <informaticien77.fr@gmail.com>
2016-09-17 12:28:08 +0200

    Added: Mac OSX detection Added: License in all files

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,3 +1,25 @@
+/* ****************************************************************************
+ *
+ * Copyright 2016 William Bonnaventure
+ *
+ * This file is part of LegendsReplay.
+ *
+ * LegendsReplay is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * LegendsReplay is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LegendsReplay.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * ****************************************************************************
+ */
+
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
@@ -1268,7 +1290,11 @@ bool MainWindow::islolRunning()
   QString output = tasklist.readAllStandardOutput();
   running = output.startsWith(QString("\"League of Legends.exe\""));
 
-#elif defined(Q_OS_UNIX)
+#elif defined(Q_OS_OSX)
+
+  log("Mac OSX is not yet supported");
+
+#elif defined(Q_OS_LINUX)
 
   tasklist.start("ps -A | grep \"League of Legends.exe\"");
   tasklist.waitForFinished();
@@ -1295,7 +1321,11 @@ bool MainWindow::islolclientRunning()
   QString output = tasklist.readAllStandardOutput();
   running = output.startsWith(QString("\"LolClient.exe\""));
 
-#elif defined(Q_OS_UNIX)
+#elif defined(Q_OS_OSX)
+
+  log("Mac OSX is not yet supported");
+
+#elif defined(Q_OS_LINUX)
 
   tasklist.start("ps -A | grep \"LolClient.exe\"");
   tasklist.waitForFinished();
