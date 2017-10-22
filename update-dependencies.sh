97e1b48dad38bc6c8cfe8bcff7d4b735d47e71e2
William Bonnaventure <william.bonnaventure@gmail.com>
2017-10-22 20:48:22 +0200

    Add custom script for Windows

---
+++ update-dependencies.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+echo "preparing the latest version of 3rd-party dependencies ...\r\n"
+
+cd qhttp
+mkdir -p 3rdparty
+cd 3rdparty
+
+echo "--> nodejs / http-parser"
+if [ -e http-parser ]; then
+    cd http-parser
+    git pull origin master
+else
+    git clone https://github.com/nodejs/http-parser.git
+fi
+
+cd ..
+cd ..
+echo "\r\ndone."
